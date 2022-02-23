45064413e78401c1d0808d5259a7ccba88dd0910
fabiocaettano <fabio.caettano74@gmail.com>
2022-02-23 19:08:55 +0000

    Fortmatando strine e valores

--- course/js/main.js
+++ course/js/main.js
@@ -1,7 +1,7 @@
 var list = [
-    {"desc":"rice","amount":"1","value":"5.40"},
+    {"desc":"rice","amount":"1","value":"4.4"},
     {"desc":"beer","amount":"12","value":"1.99"},
-    {"desc":"meat","amount":"1","value":"15.00"}
+    {"desc":"meat","amount":"1","value":"16.0"}
 ];
 
 function getTotal(list){
@@ -15,12 +15,24 @@ function getTotal(list){
 function setList(list){    
     var table = '<thead><tr><td>Description</td><td>Amout</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for(var  key in list){
-        table += '<tr><td>'+ list[key].desc +'</td><td>'+ list[key].amount +'</td><td>'+ list[key].value +'</td><td> Edit | Delete </td></tr>'
+        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td> Edit | Delete </td></tr>'
     }    
     table += '</tbody>';    
     document.getElementById("listTable").innerHTML = table;
 }
 
+function formatDesc(desc){
+    var str = desc.toLowerCase();
+    str = str.charAt(0).toUpperCase() + str.slice(1);    
+    return str;
+}
+
+function formatValue(value){
+    var str = parseFloat(value).toFixed(2) + "";
+    str = str.replace(".",",");
+    str = "$ " + str;
+    return str;
+}
 
 setList(list);
 
