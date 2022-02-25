07ddf68a11d851a2d7056238a1b4225f4b7edfad
fabiocaettano <fabio.caettano74@gmail.com>
2022-02-25 02:09:18 +0000

    validar formulario

--- course/js/main.js
+++ course/js/main.js
@@ -15,7 +15,7 @@ function getTotal(list){
 function setList(list){    
     var table = '<thead><tr><td>Description</td><td>Amout</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for(var  key in list){
-        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button onclick="setUpdate('+key+')">Edit</button><button onclick="deleteData('+key+')">Delete</button></td></tr>'
+        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+formatAmount(list[key].amount) +'</td><td>'+ formatValue(list[key].value) +'</td><td><button onclick="setUpdate('+key+')">Edit</button><button onclick="deleteData('+key+')">Delete</button></td></tr>'
     }    
     table += '</tbody>';    
     document.getElementById("listTable").innerHTML = table;
@@ -27,6 +27,10 @@ function formatDesc(desc){
     return str;
 }
 
+function formatAmount(amount){
+    return parseInt(amount);
+}
+
 function formatValue(value){
     var str = parseFloat(value).toFixed(2) + "";
     str = str.replace(".",",");
@@ -35,6 +39,9 @@ function formatValue(value){
 }
 
 function addData(){
+    if(!validation()){
+        return;
+    }
     var desc =  document.getElementById("desc").value;
     var amount = document.getElementById("amount").value;
     var value = document.getElementById("value").value;
@@ -60,9 +67,13 @@ function resetForm(){
     document.getElementById("btnUpdate").style.display = "none";
     document.getElementById("btnAdd").style.display = "inline-block";
     document.getElementById("inputIDUpdate").innerHTML = "";
+    document.getElementById("errors").style.display = "none";
 }
 
 function updateData(){
+    if(!validation()){
+        return;
+    }
     var id = document.getElementById("idUpdate").value;
     var desc = document.getElementById("desc").value;
     var amount = document.getElementById("amount").value;
@@ -91,6 +102,43 @@ function deleteData(key){
         setList(list);
     }
 
+}
+
+function validation(){
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+    var errors = "";
+
+    if (desc === ""){
+        errors += '<p>Fill out description</p>';
+    }
+
+    if (amount === ""){
+        errors += '<p>Fill out amount</p>';        
+    }else if (amount != parseInt(amount)){
+        errors += '<p>Fill out a valid amount</p>';
+    }
+
+    if (value === ""){
+        errors += '<p>Fill out amount</p>';        
+    }else if (value != parseFloat(value)){
+        errors += '<p>Fill out a valid value</p>';
+    }
+
+    if (errors != ""){
+        document.getElementById("errors").style.display = "block";
+        document.getElementById("errors").style.backgroundColor = "rgba(85,85,85,0.3)";
+        document.getElementById("errors").style.color = "white";
+        document.getElementById("errors").style.padding = "10px";
+        document.getElementById("errors").style.margin = "10px";
+        document.getElementById("errors").style.borderRadius = "13px";
+        document.getElementById("errors").innerHTML = "<h3>Error:</h3>" + errors;
+        return 0;
+    }else{ 
+        document.getElementById("errors").style.display = "none";
+        return 1;
+    }
 }
 
 setList(list);
